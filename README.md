79cc4200b86ea9096ab1da9e21f514230b1ffa1a
Jarron <jarronanthonipillai@gmail.com>
2022-11-16 17:07:39 -0500

    clean up code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rust-Bevy-Learning
 
 This project is a very basic implementation of space invaders done in order to brush up on rust as well as learn more about the bevy engine 
+
+This is built using bevy 0.9 (released 11/12/22)
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ use bevy::prelude::*;
 //assets
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const PLAYER_SIZE: (f32,f32) = (144., 75.);
+const SPRITE_SCALE: f32 = 0.5;
+
+
+//this is a resource that holds the window size
+//the # stuff is needed as of bevy 0.9
+#[derive(Resource)]
+pub struct WinSize {
+    pub w: f32,
+    pub h: f32,
+}
 
 fn main() {
     //this stuff creates the app, builds a window and calls our setup
@@ -20,18 +30,40 @@ fn main() {
         ..default()
     }))
     .add_startup_system(setup_system)
+    .add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
     .run();
 }
 
 //this function handles inital setup of things such as camera 
-fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>, mut windows: ResMut<Windows>) {
     //camera 
     commands.spawn(Camera2dBundle::default());
 
+    //get window size
+    let window = windows.get_primary_mut().unwrap();
+    let (win_w, win_h) = (window.width(), window.height());
+
+    let win_size = WinSize {
+        w: win_w,
+        h: win_h,
+    };
+    commands.insert_resource(win_size);
+
+    //position window 
+    //window.set_position(MonitorSelection::Primary, IVec2::new(2780,4900));
+}
+
+fn player_spawn_system(mut commands: Commands, asset_server: Res<AssetServer>,  win_size: Res<WinSize>) {
+    
     //add rect
+    let bottom = -win_size.h/2.0;
     commands.spawn(SpriteBundle {
        texture: asset_server.load(PLAYER_SPRITE),
-       
+        transform: Transform {
+            translation: Vec3::new(0.0, bottom+ PLAYER_SIZE.1 / 2. * SPRITE_SCALE + 5. , 10.),
+            scale: Vec3::new(SPRITE_SCALE,SPRITE_SCALE,1.),
+            ..default()
+        },
         ..default()
     });
 }
